d9bd5f4cabad70dd998d9550176542d82ac418f3
Jorge Quilcate <quilcate.jorge@gmail.com>
2017-01-10 01:43:32 +0100

    move to ansible

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i "s|^\(zookeeper\.connect\s*=\s*\).*\$|\1$ZOOKEEPER_CONNECT|" config/server.properties
-sed -i "s|^\(broker\.id\s*=\s*\).*\$|#broker\.id=$BROKER_ID|" config/server.properties
+sed -i "s|^\(broker\.id\s*=\s*\).*\$|#broker\.id=|" config/server.properties
 sed -i "s|\(log\.dirs=\).*\$|\1${LOG_DIRS}|" config/server.properties
 sed -i "s|^\(num\.partitions\s*=\).*\$|\1$NUM_PARTITION|" config/server.properties
 
