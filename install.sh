4abb8ccc096bee1237d39de478b1cd8dea840ed4
Jorge Quilcate <quilcate.jorge@gmail.com>
2017-01-12 19:53:00 +0100

    Add Zookeeper image

--- install.sh
+++
@@ -1,4 +0,0 @@
-curl -o $KAFKA_PKG.tgz $KAFKA_URL
-tar xf $KAFKA_PKG.tgz
-rm -f $KAFKA_PKG.tgz
-mv $KAFKA_PKG $KAFKA_HOME
--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -5,5 +5,7 @@ sed -i "s|^\(zookeeper\.connect\s*=\s*\).*\$|\1$ZOOKEEPER_CONNECT|" config/serve
 sed -i "s|^\(broker\.id\s*=\s*\).*\$|#broker\.id=|" config/server.properties
 sed -i "s|\(log\.dirs=\).*\$|\1${KAFKA_LOGS}|" config/server.properties
 sed -i "s|^\(num\.partitions\s*=\).*\$|\1$NUM_PARTITION|" config/server.properties
+sed -i "s|^\(#listeners\s*=\).*\$|listeners=PLAINTEXT://$HOSTNAME:$KAFKA_PORT|" config/server.properties
+sed -i "s|^\(#advertised\.listeners\s*=\).*\$|advertised\.listeners=PLAINTEXT://$HOSTNAME:$KAFKA_PORT|" config/server.properties
 
 exec "$@"
